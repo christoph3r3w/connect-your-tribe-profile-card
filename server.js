506139961bc52f1597d06d988085e1002f40a356
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-02-06 14:28:33 +0100

    v1

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ import express from 'express'
 import fetchJson from './helpers/fetch-json.js'
 
 // Haal data op uit de FDND API, ga pas verder als de data gedownload is
-const data = await fetchJson('https://fdnd.directus.app/items/person/40')
-console.log(data); // uncomment om de opgehaalde data te checken
+const {data} = await fetchJson('https://fdnd.directus.app/items/person/40')
+// const {data} = await fetchJson('https://fdnd.directus.app/items/person/40')
+console.log(data.nickname); // uncomment om de opgehaalde data te checken
 
 // Maak een nieuwe express app aan
 const app = express()
