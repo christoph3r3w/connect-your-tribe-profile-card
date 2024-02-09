ce1c972070c13a85f295644914ad537ffbe66b85
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-02-09 10:47:01 +0100

    v2.1

---
+++ public/scripts/general.js
@@ -0,0 +1,25 @@
+const body = document.querySelector('body');
+const screen = document.querySelector('screen');
+const B_start = document.querySelector('.start');
+const theme = document.querySelector('.window_theme');
+// restart button
+// const RS_B = document.querySelector('');
+var bentana = document.querySelector('div.window_card').id;
+const exit = document.querySelector('.exit');
+
+// ask about dynamic atribute
+exit.addEventListener(click,(e=>{
+
+}))
+
+console.log(bentana);
+// let check = function(){
+// if (bentana === 'personal') { 
+//      exit.setAttribute("popovertarget","personal")
+//  } else if (bentana === 'sprint') { 
+//     exit.setAttribute("popovertarget","sprint") 
+//  } else if (bentana === 'music') { 
+//     exit.setAttribute("popovertarget","music") 
+// } else { console.log("pop")  }
+// };
+// check();
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import express from 'express'
 // Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
 import fetchJson from './helpers/fetch-json.js'
 
+
+
 // Haal data op uit de FDND API, ga pas verder als de data gedownload is
 const {data} = await fetchJson('https://fdnd.directus.app/items/person/40')
 // const {data} = await fetchJson('https://fdnd.directus.app/items/person/40')
